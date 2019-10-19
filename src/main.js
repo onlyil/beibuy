9fcce0949f3878532c5359302874b46153c9a7f3
onlyil <lin.cao@beibei.com>
2019-10-19 11:03:20 +0800

    Update: rem

--- src/main.js
+++ src/main.js
@@ -4,6 +4,35 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+// 自适应布局，pc 端不计算 rem，固定为 640 / 16
+! function (e) {
+  function t() {
+      var ua = e.navigator.userAgent;
+      if (ua.indexOf('iPhone') > -1 || ua.indexOf('Android') > -1) {
+          e.rem = m.getBoundingClientRect().width / 16, m.style.fontSize = e.rem + "px"
+      } else {
+          e.rem = 640 / 16;
+          m.style.fontSize = e.rem + "px";
+      }
+  }
+  var i, n = e.navigator.appVersion.match(/iphone/gi) ? e.devicePixelRatio : 1,
+      a = 1 / n,
+      m = document.documentElement,
+      r = document.createElement("meta");
+  if (e.dpr = n, e.addEventListener("resize", function () {
+          clearTimeout(i), i = setTimeout(t, 300)
+      }, !1), e.addEventListener("pageshow", function (e) {
+          e.persisted && (clearTimeout(i), i = setTimeout(t, 300))
+      }, !1), m.setAttribute("data-dpr", n), r.setAttribute("name", "viewport"), r.setAttribute("content",
+          "initial-scale=" + a + ", maximum-scale=" + a + ", minimum-scale=" + a + ", user-scalable=no"), m
+      .firstElementChild) m.firstElementChild.appendChild(r);
+  else {
+      var d = document.createElement("div");
+      d.appendChild(r), document.write(d.innerHTML)
+  }
+  t()
+}(window)
+
 Vue.config.productionTip = false
 
 new Vue({
